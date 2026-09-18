8e70fed245e565e8a508c4a1099eb42ca8169872
maximhelmerotto <maxim.helmer@otto.de>
2026-09-18 14:49:16 +0200

    final final

--- yahoo_anbindung.py
+++ yahoo_anbindung.py
@@ -17,23 +17,24 @@ class get_data:
         self.ist_aktie = quote.get("quoteType") in ("EQUITY", "STOCK")
         self.ist_derivat = quote.get("quoteType") in ("OPTION", "FUTURE", "FUTURES")
         self.aktie = yf.Ticker(self.ticker)
-        
-        self.währung = "GBP" if self.aktie.fast_info['currency'] == "GBp" else self.aktie.fast_info['currency']
-        self.preis = converter.convert(self.aktie.fast_info['last_price'] / 100 if self.aktie.fast_info['currency'] == "GBp" else self.aktie.fast_info['last_price'], self.währung, 'EUR')
+
+        self.original_währung = "GBP" if self.aktie.fast_info['currency'] == "GBp" else self.aktie.fast_info['currency']
         self.währung = "EUR"
+        self.preis = converter.convert(self.aktie.fast_info['last_price'] / 100 if self.aktie.fast_info['currency'] == "GBp" else self.aktie.fast_info['last_price'], self.original_währung, 'EUR')
+        
         
         self.historie = self.aktie.history(period="1y")[['Close']]
         
 
         if self.ist_aktie:
-            self.marktkapitalisierung = converter.convert(self.aktie.fast_info['market_cap'], self.währung, 'EUR')
+            self.marktkapitalisierung = converter.convert(self.aktie.fast_info['market_cap'], self.original_währung, "EUR")
             
         else:
             fondgröße = (self.aktie.info.get('totalAssets') or
                           self.aktie.info.get('netAssets') or
                           quote.get('totalAssets') or
                           quote.get('netAssets'))
-            self.fondgröße = converter.convert(fondgröße, self.währung, 'EUR') if fondgröße is not None else None
+            self.fondgröße = converter.convert(fondgröße, self.original_währung, 'EUR') if fondgröße is not None else None
         
         if self.ist_derivat:
             
@@ -43,7 +44,7 @@ class get_data:
                 original_waehrung,
                 "EUR")
             basiswert = yf.Ticker(self.aktie.info['underlyingSymbol'])
-            self.basiswert_preis = converter.convert(basiswert.fast_info['last_price'], basiswert.fast_info['currency'])
+            self.basiswert_preis = converter.convert(basiswert.fast_info['last_price'], basiswert.fast_info['currency'], "EUR")
             self.hebel= self.basiswert_preis / self.derivat_preis
             
             
